de9d96d1b7afbcb5096e679c47aaf9600bcd1abc
praveen2111 <54503215+praveen2111@users.noreply.github.com>
2021-01-25 18:21:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import {useState, useEffect} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(true)
+
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async() =>{
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    
+    getTasks()
+  }, [] )
+
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data= await res.json()
+    return data
+  }
+
+  // Add Task
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks',{
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+    const data=await res.json()
+
+    setTasks([...tasks,data])
+
+
+
+
+
+
+    // const id = Math.floor(Math.random()*10000)+1;
+    // const newTask = {id, ...task}
+    // setTasks([...tasks,newTask])
+  }
+
+  //Delete task
+  const deleteTask = async(id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle Remainder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} :task))
+  }
+  
+  return (
+    <div className="container">
+      <Header onAdd = {() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? 
+      (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>): 
+      ('No Tasks To Show')}
+    </div>
+  );
+}
+
+export default App;
